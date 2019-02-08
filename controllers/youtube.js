d3aac00e0bdcdd069267cd80ded7f6372a6ff987
fauzan_bintang <obin.aji@gmail.com>
2019-02-08 15:06:36 +0700

    fix youtube response result

--- controllers/youtube.js
+++ controllers/youtube.js
@@ -3,9 +3,12 @@ const axios = require('axios')
 module.exports = {
     getVideos(req, res) {
         axios
-            .get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${req.query.search}&key=${process.env.YOUTUBE_KEY}&type=video`)
+            .get(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${req.query.search}&key=${process.env.YOUTUBE_KEY}&type=video&maxResult=3`)
             .then(({data}) => {
-                res.status(200).json(data)
+                let result = data.items.map(item => {
+                    return item.id.videoId
+                })
+                res.status(200).json({result})
             })
             .catch(err => {
                 res.status(500).json({
