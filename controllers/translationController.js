1b60c4ba167d2fc50a1e0356eb5e54187f1e40cd
Mahdihrs <mahdiharis@outlook.com>
2019-02-08 09:06:28 +0700

    google-translate

---
+++ controllers/translationController.js
@@ -0,0 +1,40 @@
+const googleTranslate = require('google-translate')(process.env.GOOGLE_TRANSLATE_KEY);
+
+class TranslationController {
+    static translateToIndo(req, res) {
+        // console.log(req.body.words)
+        googleTranslate.translate(req.body.words, 'id', function(err, translation) {
+            if (err) {
+                res
+                    .status(500)
+                    .json({
+                        msg: `Internal Server Error`,
+                        err: err
+                    })
+            } else {
+                res
+                    .status(200)
+                    .json(translation)
+            }
+        });
+    }
+
+    static getAllLangs(req, res) {
+        googleTranslate.getSupportedLanguages('de', function(err, languageCodes) {
+            if (err) {
+                res
+                    .status(500)
+                    .json({
+                        msg: `Internal Server Error`,
+                        err: err
+                    })                 
+            } else {
+                res
+                    .status(200)
+                    .json(languageCodes)
+            }
+        });
+    }
+}
+
+module.exports = TranslationController
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,12 @@ var express = require('express');
 var router = express.Router();
 const user = require('./users')
 const recipeRoute = require('./recipe')
+const translateRouter = require('./translate')
 
 /* GET home page. */
 
 router.use('/', user)
 router.use('/recipe', recipeRoute)
+router.use('/translate', translateRouter)
 
 module.exports = router;
